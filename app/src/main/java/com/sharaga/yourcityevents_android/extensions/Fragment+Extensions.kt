0110b26ec3e1b2b665c34897e8c95f17e5e55980
Taras <kondratiuk.taras123@gmail.com>
2019-11-27 00:23:39 +0200

    changes in displaying fragments

--- app/src/main/java/com/sharaga/yourcityevents_android/extensions/Fragment+Extensions.kt
+++ app/src/main/java/com/sharaga/yourcityevents_android/extensions/Fragment+Extensions.kt
@@ -1,18 +1,19 @@
 package com.sharaga.yourcityevents_android.extensions
 
-import android.app.Activity
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 
-fun Activity.showError(error: String) {
-    val builder = AlertDialog.Builder(this)
+fun Fragment.showError(error: String) {
+    val act = activity?:return
+    val builder = AlertDialog.Builder(act)
     builder.setMessage("$error")
     builder.setPositiveButton(android.R.string.yes) { _, _ -> }
     builder.show()
 }
 
-fun Activity.show(message: String) {
-    val builder = AlertDialog.Builder(this)
-    builder.setMessage("$message")
-    builder.setPositiveButton(android.R.string.yes) { _, _ -> }
-    builder.show()
+fun Fragment.show(message: String) {
+//    val builder = AlertDialog.Builder(activity!!.applicationContext)
+//    builder.setMessage("$message")
+//    builder.setPositiveButton(android.R.string.yes) { _, _ -> }
+//    builder.show()
 }
--- app/src/main/java/com/sharaga/yourcityevents_android/modules/auth/login/view/LoginView.kt
+++ app/src/main/java/com/sharaga/yourcityevents_android/modules/auth/login/view/LoginView.kt
@@ -1,7 +1,5 @@
 package com.sharaga.yourcityevents_android.modules.auth.login.view
 
-import android.app.PendingIntent.getActivity
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.sharaga.yourcityevents_android.R
 import com.sharaga.yourcityevents_android.modules.auth.login.presenter.LoginPresenter
-import com.sharaga.yourcityevents_android.modules.mainbar.MainBarActivity
 import kotlinx.android.synthetic.main.login_fragment1.*
 import java.lang.ref.WeakReference
 
@@ -20,28 +17,46 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_fragment1)
-        this.presenter = LoginPresenter(WeakReference(this))
+        setContentView(R.layout.login_activity)
 
-        sign_in.setOnClickListener {
-            val intent = Intent(this, MainBarActivity::class.java)
-//            val intent = Intent(this, MainBarActivity.class)
-            startActivity(intent)
-        }
 
-        sign_up.setOnClickListener {
+        loadFragment(LoginFragment())
 
-        }
+
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.loginContainer, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
 
 
-class LoginFragment1 : Fragment() {
+class LoginFragment : Fragment() {
+    lateinit var presenter: LoginPresenter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment1, container, false)
+
+        val view = inflater.inflate(R.layout.login_fragment1, container, false)
+        this.presenter = LoginPresenter(WeakReference(this))
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sign_in.setOnClickListener {
+//            val intent = Intent(activity, MainBarActivity::class.java)
+//            startActivity(intent)
+            presenter.login("","")
+        }
     }
 }
