cb4ae0888f59ee9a79aaca1bdde37755260e4ed1
Taras <kondratiuk.taras123@gmail.com>
2019-12-01 15:59:54 +0200

    fixed wrong RecyclerView layout element

--- app/src/main/java/com/sharaga/yourcityevents_android/modules/mainbar/feed/FeedFragment.kt
+++ app/src/main/java/com/sharaga/yourcityevents_android/modules/mainbar/feed/FeedFragment.kt
@@ -5,11 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.sharaga.yourcityevents_android.R
+import kotlinx.android.synthetic.main.fragment_feed.*
 
-class FeedFragment : Fragment(){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_feed,container,false)
 
+class FeedFragment : Fragment() {
+    private lateinit var eventsRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+
+    }
+
+    private fun initRecyclerView() {
+        eventsRecyclerView = recycler as RecyclerView
     }
 }
